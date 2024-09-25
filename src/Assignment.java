c979ac169664fc22c0ee21ddb6b3b81c48ca64b7
WahidHoquee <wahidhoquee@gmail.com>
2024-09-26 03:47:50 +0600

    feat: question 2

--- src/Assignment.java
+++ src/Assignment.java
@@ -2,7 +2,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.CompilationUnit;
@@ -10,20 +12,34 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 public class Assignment {
 	public static void main(String[] args) throws IOException {
 		// Reading source file
-		String relativePath = "test/ContentReader.java";
-		Path path = Paths.get(relativePath);
+		String directory = "test";
+		String file = "ContentReader.java";
+		Path path = Paths.get(directory + "/" + file);
 		String content = Files.readString(path);
 
 		// Parses Java source code from content into an AST
 		ASTParser parser = ASTParser.newParser(AST.getJLSLatest());
 		parser.setSource(content.toCharArray());
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setResolveBindings(true);
+		parser.setBindingsRecovery(true);
+
+		Map<String, String> options = JavaCore.getOptions();
+		parser.setCompilerOptions(options);
+		parser.setUnitName(file);
+		String[] sources = { Paths.get("src").toString() };
+		parser.setEnvironment(null, sources, new String[] { "UTF-8" }, true);
 
 		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
 
+		System.out.println("Question-1");
+		System.out.println("---------------------------------------------------");
 		new Assignment_1(cu);
 
-		// new Assignment_2();
+		System.out.println("Question-2");
+		System.out.println("---------------------------------------------------");
+		new Assignment_2(cu);
+
 		// new Assignment_3();
 		// new Assignment_4();
 	}
--- src/Assignment_2.java
+++ src/Assignment_2.java
@@ -1,71 +1,39 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Expression;
-import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.MethodInvocation;
-import org.eclipse.jdt.core.dom.MethodRef;
-import org.eclipse.jdt.core.dom.MethodRefParameter;
-import org.eclipse.jdt.core.dom.SimpleName;
 
 public class Assignment_2 {
-	public Assignment_2() throws IOException {
-		String relativePath = "test/ContentReader.java";
-		Path path = Paths.get(relativePath);
-		String content = Files.readString(path);
-
-		ASTParser parser = ASTParser.newParser(AST.JLS8);
-		parser.setSource(content.toCharArray());
-		parser.setKind(ASTParser.K_COMPILATION_UNIT);
-		parser.setResolveBindings(true);
-		parser.setBindingsRecovery(true);
-
-		Map<String, String> options = JavaCore.getOptions();
-		parser.setCompilerOptions(options);
-
-		String unitName = "ContentReader.java";
-		parser.setUnitName(unitName);
-
-		String[] sources = { "C:\\Users\\wahid\\eclipse-workspace\\Javaparser\\src" };
-
-		parser.setEnvironment(null, sources, new String[] { "UTF-8" }, true);
-		parser.setSource(content.toCharArray());
-
-		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
-
+	public Assignment_2(CompilationUnit cu){
 		ASTVisitor tree = new ASTVisitor() {
 			@Override
 			public boolean visit(MethodInvocation node) {
+                // Initialize lists to hold the parameters and their types
 				ArrayList<String> params = new ArrayList<>();
 				ArrayList<String> types = new ArrayList<>();
 				for (Object arg : node.arguments()) {
 					Expression expression = (Expression) arg;
 					ITypeBinding typeBinding = expression.resolveTypeBinding();
+					// If the type binding is not null, store the expression and its type
 					if (typeBinding != null) {
 						params.add(expression.toString());
 						types.add(typeBinding.getName());
 					}
 				}
-				System.out.printf("Line: %d Method Declaration: %s.%s(%s)%n", cu.getLineNumber(node.getStartPosition()),
-						node.getExpression().toString(), node.getName().getIdentifier(), String.join(",", params));
-				System.out.printf("Method Signature: %s:%d:%s%n", node.getName(), node.arguments().size(),
-						String.join(",", types));
-				System.out.println();
-				return super.visit(node);
 
+				int line = cu.getLineNumber(node.getStartPosition());
+				int noOfArgs = node.arguments().size();
+				String identifier = node.getExpression().toString();
+				String methodCall = node.getName().getIdentifier();
+
+				System.out.printf("Line Number: %d Method Call: %s.%s(%s)%n", line, identifier, methodCall,
+						String.join(",", params));
+				System.out.printf("Method Signature: %s:%d:%s%n", methodCall, noOfArgs, String.join(",", types));
+				System.out.println();
+
+				return super.visit(node);
 			}
 		};
 
